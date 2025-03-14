57d1909252e470e621191ba5804db952d079b5ca
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-03-14 19:37:03 +0900

    서버 설정파일 업데이트

---
+++ backend/demo/src/main/java/com/todo/demo/DodnggukApplication.java
@@ -0,0 +1,13 @@
+package com.todo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodnggukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodnggukApplication.class, args);
+	}
+
+}
