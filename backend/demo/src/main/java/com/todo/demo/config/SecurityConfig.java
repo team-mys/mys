0b0eb57c94913ffb8813730513b560f0eeaada39
손원익 <102235796+DDungEEEE@users.noreply.github.com>
2025-03-30 21:34:27 +0900

    refeat: update SecurityConfig

--- backend/demo/src/main/java/com/todo/demo/config/SecurityConfig.java
+++ backend/demo/src/main/java/com/todo/demo/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of("http://localhost:5173")); // '*' 쓰면 allowCredentials 안 됨
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
